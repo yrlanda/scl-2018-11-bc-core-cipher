6424561a83e64958a34572029b8dca74709831ed
Yrlanda <yrlaterivero@gmal.com>
2018-12-01 17:04:35 -0300

    ya funciona los botones de codificar y decodificar por las funciones que determine en el cipher

--- cipher.js
+++ cipher.js
@@ -1,8 +1,33 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
+  encode: (mensaje, offset) => {
+    let mensaje_codificado = "";
+
+    for (let i = 0; i < mensaje.length; i++) {
+      let ascii_letra_mensaje = mensaje.toUpperCase().charCodeAt(i);
+      if (ascii_letra_mensaje >= 65 && ascii_letra_mensaje <= 90) {
+
+        ascii_letra_mensaje = ((ascii_letra_mensaje - 65 + offset) % 26) + 65;
+
+      }
+      mensaje_codificado = mensaje_codificado + String.fromCharCode(ascii_letra_mensaje);
+
+    }
+    return mensaje_codificado;
   },
-  decode: () => {
-    /* Acá va tu código */
+
+
+  decode: (mensaje, offset) => {
+    let mensaje_decodificado = "";
+
+    for (let i = 0; i < mensaje.length; i++) {
+      let ascii_letra_mensaje = mensaje.toUpperCase().charCodeAt(i);
+      if (ascii_letra_mensaje >= 65 && ascii_letra_mensaje <= 90) {
+
+        ascii_letra_mensaje = ((ascii_letra_mensaje - 65 - offset) % 26) + 65;
+      }
+      mensaje_decodificado = mensaje_decodificado + String.fromCharCode(ascii_letra_mensaje);
+
+    }
+    return mensaje_decodificado;
   }
 };
--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-/* Acá va tu código */
+window.onload = () => {
+    const box_mensaje = document.getElementById('mensaje');
+    const box_offset = document.getElementById('desplazamientos');
+
+
+    document.getElementById('codificar').addEventListener('click', (evento) => {
+        evento.preventDefault();
+        let entrada = box_mensaje.value;
+        let offset = parseInt(box_offset.value);
+
+        console.log(cipher.encode(entrada, offset));
+
+    });
+
+    document.getElementById('decodificar').addEventListener('click', (evento) => {
+        evento.preventDefault();
+
+        let entrada = box_mensaje.value;
+        let offset = parseInt(box_offset.value);
+
+        console.log(cipher.decode(entrada, offset));
+
+
+    });
+}
+
+
